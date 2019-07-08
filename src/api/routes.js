34bfd20840f541dc6f28fc46787ff704d97f6abe
eqbit <eqbits@gmail.com>
2019-07-09 01:17:24 +0500

    main-page

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,8 +1,8 @@
 const routes = {
-  menu: 'http://test.eqbit.ru/wp-json/menus/v1/menus/main_menu',
-  posts: 'http://test.eqbit.ru/wp-json/sections/v1/post',
-  portfolio: 'http://test.eqbit.ru/wp-json/portfolio/v1/post',
-  portfolioSingle : 'http://test.eqbit.ru/wp-json/portfolio/v1/post?slug='
+  menu: 'http://wp.web-dev-studio.ru/wp-json/menus/v1/menus/main_menu',
+  posts: 'http://wp.web-dev-studio.ru/wp-json/sections/v1/post',
+  portfolio: 'http://wp.web-dev-studio.ru/wp-json/portfolio/v1/post',
+  portfolioSingle : 'http://wp.web-dev-studio.ru/wp-json/portfolio/v1/post?slug='
 };
 
 export {routes}
