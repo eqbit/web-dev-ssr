3571c62b404dba57a25aa7d5f49f7db13a5a3725
eqbit <eqbits@gmail.com>
2019-07-18 15:35:10 +0500

    vk vidget

--- src/components/vk-vidget/index.js
+++ src/components/vk-vidget/index.js
@@ -1,6 +1,6 @@
 class VkVidget extends React.Component {
   componentDidMount() {
-    VK.Widgets.CommunityMessages("vk_community_messages", 183139911, {expanded: "1",tooltipButtonText: "Есть вопрос?"});
+    VK.Widgets.CommunityMessages("vk_community_messages", 183139911, {expanded: "0",tooltipButtonText: "Есть вопрос?"});
   }
   
   render() {
