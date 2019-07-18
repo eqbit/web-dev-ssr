e70c90b0700285157260b58fd7fa4aae11cb6161
eqbit <eqbits@gmail.com>
2019-07-18 15:40:17 +0500

    vk vidget

--- src/components/vk-vidget/index.js
+++ src/components/vk-vidget/index.js
@@ -2,8 +2,7 @@ class VkVidget extends React.Component {
   componentDidMount() {
     VK.Widgets.CommunityMessages("vk_community_messages", 183139911, {
       disableExpandChatSound: "1",
-      disableNewMessagesSound: "1",
-      disableButtonTooltip: "1"
+      tooltipButtonText: "напишите нам в vk"
     });
   }
   
