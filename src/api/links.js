a481f7e756ccb8d7928c3b61d02f135a979f1a68
eqbit <eqbits@gmail.com>
2019-07-18 15:01:24 +0500

    add required email to modals

--- src/api/links.js
+++ src/api/links.js
@@ -1,11 +1,18 @@
 const be = 'https://www.behance.net/Mikhail_Bulatov';
+
 const phone = {
-  link: 'tel:+79523473419',
-  title: <span>+7 (952) <b>347-34-19</b></span>
+  link: 'tel:+79028137911',
+  title: <span>+7 (902) <b>813-79-11</b></span>
 };
+
 const mail = {
-  link: 'mailto:web.d3v@yandex.ru',
-  title: <span>web.d3v@yandex.ru</span>
+  link: 'mailto:info@web-dev-studio.ru',
+  title: <span>info@web-dev-studio.ru</span>
 };
 
 export {be, phone, mail}
+
+/*const phone = {
+  link: 'tel:+79523473419',
+  title: <span>+7 (952) <b>347-34-19</b></span>
+};*/
